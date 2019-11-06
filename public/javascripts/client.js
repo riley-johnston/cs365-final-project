a129e5be93c6a7982ab840ecf73f838b76d6bee9
AnnaMarie1714 <amdamico17@gmail.com>
2019-11-06 10:20:54 -0500

    Methods AD

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -18,19 +18,30 @@ var v = new Vue({
     
     methods: {
         changeResult(column, row, col){
+            var color = "N/A";
             if(/*There is a ship there*/ 0 == 0){
                if(/*The ship has been sunk*/0 == 0){
-
+                   /*My color and the colors of the ships around me turn black*/
                } else {
                    /*The ship has not been sunk*/
+                   color = "Red";
                }
             } else {
                 /*There is no ship there*/
+                color = "White";
             }
-            this.results[col].splice(row, 1, opposite);
-            this.results[row].splice(col, 1, column);
+            this.results[col].splice(row, 1, color);
             $.post("/updateArray", {updatedResults: this.results}, function(){
             });
+        },
+        forfeit(){
+
+        },
+        randomPlacement(){
+
+        },
+        ready(){
+            
         }
     }
 })
