34f9fb7aedbcce4f14e6b75ff365c384198cabe1
AnnaMarie1714 <amdamico17@gmail.com>
2019-11-08 10:45:58 -0500

    11/08/19 AD

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -30,19 +30,22 @@ var v = new Vue({
     },
     methods: {
         changeResult(row, col){
+            this.color = document.getElementById("myTable1").getElementsByClassName("gridbtn"); /*This gets all of the buttons belonging to the class gridbtn*/
+            var index = this.myGuess[row]; /*This grabs the whole row, but as soon as I try to get the column, it is undefined.*/
+            console.log(index);
             if(this.myShips[row][col] != " "){
                if(/*The ship has been sunk*/this.color == "N/A"){
                    /*My color and the colors of the ship's squares around me turn black*/
                    this.color = "Black";
                } else {
-                   /*The ship has not been sunk*/
-                   this.color = "Red"; /*change the CSS property?*/
+                   /*The ship has not been sunk, but has been hit*/
+                   this.color[row].style.backgroundColor = "red"; /*change the CSS property?*/
                }
             } else {
                 /*There is no ship there*/
-                this.color = "White";
+                this.color[row].style.backgroundColor = "white";
             }
-            this.myShips[row].splice(col, 1, this.color);
+            //this.myShips[row].splice(col, 1, this.color);
             $.post("/updateArray", {updatedResults: this.results}, function(){
             });
         },
